c2b4bd9e326804d98b9f064f74bfc6fdfa36d1d7
2-glauciane <164278918+2-glauciane@users.noreply.github.com>
2024-11-13 21:44:37 -0300

    l

--- perguntas.js
+++ perguntas.js
@@ -1,24 +1,24 @@
 criaCartao(
     'seja feliz',
-    'oq precisa pra vc ser feliz?',
+    'o que precisa pra você ser feliz?',
     ' Fazendo oq eu gosto!!!'
 )
 
 criaCartao(
-    'qual melhor saite pra roupa',
-    ''qual a melhor loja de saite de roupa ,
+    'Compras',
+    'qual a melhor loja de site de roupa?' ,
     'shopee'
 )
 
 criaCartao(
-    'qual aplicativo para muisca  ',
-    'qual aplicativo pra escuta musicas ?',
-    'spotify'
+    'Música',
+    'qual o melhor aplicativo pra escuta musicas?',
+    'Spotify'
 )
 
 criaCartao(
-    'qual lugar ',
-    'qual o melhor lugar pra ir ?',
-        'ir para minha segunda casa'
+    'Paz',
+    'qual o melhor lugar pra ir?',
+    'ir para minha segunda casa'
 )
        moraizinha
